fd470ec2a06a9ad82e5c5cd0ec43b3241b39d2e8
oladiiposaheed <64591844+oladiiposaheed@users.noreply.github.com>
2023-02-10 17:37:41 +0300

    Update firstpython.py

--- firstpython.py
+++ firstpython.py
@@ -1,1 +1,13 @@
-print('Hello World')
+def f1(num):
+    c = 0
+    s = 0
+    for i in range(num):
+        if i % 2 == 0:
+            c += 1
+            s += i
+        print(i, end=' ')
+    print('\nTotal Even Number: ',c)
+    print('Sum: ',s)
+
+num = int(input('Enter a number'))
+f1(num)
